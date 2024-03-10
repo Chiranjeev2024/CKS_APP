0b4773660f543a741b1172662c65e35f10da6fd0
Chiranjeev2024 <sks7369064258@gmail.com>
2024-03-11 02:05:28 +0530

    adding text box

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 
 import Navbar from './component/Navbar';
+import TextForm from './component/TextForm';
+
 
 function App() {
   return (
     <>
       <Navbar title = {"TextUtils"} About = "contact"/>
+      <div className='container'>
+        <TextForm heading = "Enter The Text to Analyze"/>
+      </div>
+      
     </>
   );
 }
